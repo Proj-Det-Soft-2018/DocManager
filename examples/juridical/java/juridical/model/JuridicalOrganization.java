f18d0f3ddd0fad9344173c27f6be480d1cbc110c
Clarissa Soares <clah@ufrn.edu.br>
2018-06-19 13:58:20 -0300

    Classes modelos da instância juridical concluída
    falta ajustes na classe Lawyer

--- examples/juridical/java/juridical/model/JuridicalOrganization.java
+++ examples/juridical/java/juridical/model/JuridicalOrganization.java
@@ -3,6 +3,9 @@
  */
 package juridical.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import business.model.Organization;
 
 /**
@@ -10,25 +13,47 @@ import business.model.Organization;
  *
  */
 public enum JuridicalOrganization implements Organization {
-	NULL
+	NULL("- Inválido -","Invalido"),
+	VSNAT1("1ª Vara de Sucessões da Comarca de Natal","1ª VS Natal"),
+	VSNAT2("2ª Vara de Sucessões da Comarca de Natal","2ª VS Natal"),
+	VSNAT3("3ª Vara de Sucessões da Comarca de Natal","3ª VS Natal"),
+	VSMOS1("1ª Vara de Sucessões da Comarca de Mossoró","1ª VS Mossoró"),
 	;
-
+	
+	private String description;
+	private String initials;
+	
+	
+	private JuridicalOrganization(String description, String initials) {
+		this.description = description;
+		this.initials = initials;
+	}
+	
 	@Override
 	public String getFullName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.description;
 	}
 
 	@Override
 	public String getInitials() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.initials;
 	}
 
 	@Override
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return ordinal();
 	}
-
+	
+	public static List<Organization> getAll() {
+		List<Organization> organizationList = new ArrayList<>();
+		for(JuridicalOrganization organization : JuridicalOrganization.values()) {
+			organizationList.add(organization);
+		}
+		organizationList.remove(0);
+		return organizationList;
+	}
+	
+	public static JuridicalOrganization getOrganizationById(int id){
+		return JuridicalOrganization.values()[id];
+	}
 }
