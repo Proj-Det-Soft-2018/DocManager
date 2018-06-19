90bfc6d041c61ae9f404a4100289268f7c71d62c
Hugo Oliveira <hugotholiveira@hotmail.com>
2018-06-18 22:55:06 -0300

    Correção enum situação para processo compra

--- examples/purchase/java/purchase/model/PurchaseSituation.java
+++ examples/purchase/java/purchase/model/PurchaseSituation.java
@@ -7,20 +7,20 @@ import business.model.Situation;
 
 public enum PurchaseSituation implements Situation {
   /*   / ENUM NAME /DESCRIPTION                               /LINKED NODES*/
-  /* 0*/ NULL      (null,                                     new int[]{}),
-  /* 1*/ AGCHLOCAL ("Aguardando autorização da chefia local", new int[]{}),
-  /* 2*/ AGAUTCOMP ("Aguardando autorização para compra",	  new int[]{}),
-  /* 3*/ ANSETORCA ("Em análise no setor de orçamento",       new int[]{}),
-  /* 4*/ EMPENHADO ("Empenhado",                              new int[]{}),
-  /* 5*/ NOTFORNEC ("Notificado para o fornecedor",           new int[]{}),
-  /* 6*/ APRFORNEC ("Aprovado pelo fornecedor",               new int[]{}),
-  /* 7*/ ENVFORNEC ("Enviado pelo fornecedor",                new int[]{}),
-  /* 8*/ RECEBALMX ("Recebido pelo almoxarifado",             new int[]{}),
-  /* 9*/ PAGOLIQUD ("Pago/Liquidado",                         new int[]{}),
-  /*10*/ FORAPRAZO ("Não entregue dentro do prazo",           new int[]{}),
-  /*11*/ AUDITORIA ("Em auditoria",                           new int[]{}),
-  /*12*/ SJURIDICO ("Em análise pelo setor jurídico",         new int[]{}),
-  /*13*/ CANCELADO ("Cancelado",                              new int[]{});
+  /* 0*/ NULL      (null,                                     new int[]{0, 1}),
+  /* 1*/ AGCHLOCAL ("Aguardando autorização da chefia local", new int[]{1, 2}),
+  /* 2*/ AGAUTCOMP ("Aguardando autorização para compra",	  new int[]{1, 2, 3}),
+  /* 3*/ ANSETORCA ("Em análise no setor de orçamento",       new int[]{2, 3, 4, 11}),
+  /* 4*/ EMPENHADO ("Empenhado",                              new int[]{3, 4, 5}),
+  /* 5*/ NOTFORNEC ("Notificado para o fornecedor",           new int[]{4, 5, 6, 10}),
+  /* 6*/ APRFORNEC ("Aprovado pelo fornecedor",               new int[]{5, 6, 7, 10}),
+  /* 7*/ ENVFORNEC ("Enviado pelo fornecedor",                new int[]{6, 7, 8, 10}),
+  /* 8*/ RECEBALMX ("Recebido pelo almoxarifado",             new int[]{7, 8, 9, 10}),
+  /* 9*/ PAGOLIQUD ("Pago/Liquidado",                         new int[]{8, 9}),
+  /*10*/ FORAPRAZO ("Não entregue dentro do prazo",           new int[]{5, 6, 7, 8, 10, 12}),
+  /*11*/ AUDITORIA ("Em auditoria",                           new int[]{3, 11, 12}),
+  /*12*/ SJURIDICO ("Em análise pelo setor jurídico",         new int[]{10, 11, 12, 13}),
+  /*13*/ CANCELADO ("Cancelado",                              new int[]{12, 13});
 	
 	private String description;
 
